8861055cde91c03e5f1a00a7383cfce08152350d
Jan Pearce <pearcej@gmail.com>
2021-04-15 08:21:17 -0400

    Create ciphers.py

---
+++ ciphers.py
@@ -0,0 +1,11 @@
+"""Encoding and Decoding Cyphers
+    By FIXME"""
+
+"""Create functions that will encode and decode using a variety of cyphers"""
+
+# FIXME - functions here (use appropriate parameters like P and K to return C, etc)
+
+def main():
+    # calls to functions and printing here
+
+main()
